489f201b69a015343fe074497525e8e289aaf1e1
unknown <khaledgaber494@gmail.com>
2026-03-05 16:28:04 +0200

    first commit

--- store/urls.py
+++ store/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
-    path('onlinestore/', include('onlinestore.urls')),
+    path('', include('onlinestore.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
