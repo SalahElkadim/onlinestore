6221d209a46060fdbe4a51e591d9a6380e6e359b
unknown <khaledgaber494@gmail.com>
2026-03-05 15:56:52 +0200

    first commit

--- store/settings.py
+++ store/settings.py
@@ -190,7 +190,7 @@ SIMPLE_JWT = {
 # Development & Production
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS', 
-    'http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001'
+    'http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001,https://trendshop-production.up.railway.app,https://admin-dashboard-production-cdaa.up.railway.app'
 ).split(',')
 
 CORS_ALLOW_CREDENTIALS = True
