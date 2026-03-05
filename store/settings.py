2ec8fabe5fc03bbf9cd8d4c216e3b698eae1b903
unknown <khaledgaber494@gmail.com>
2026-03-05 16:59:02 +0200

    first commit

--- store/settings.py
+++ store/settings.py
@@ -190,7 +190,7 @@ CORS_ALLOWED_ORIGINS = os.getenv(
     'http://localhost:3000,http://127.0.0.1:3000,http://localhost:3001,http://127.0.0.1:3001,https://trendshop-production.up.railway.app,https://admin-dashboard-production-cdaa.up.railway.app'
 ).split(',')
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 CORS_ALLOW_METHODS = [
     'DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT',
